0eae1d5c5e3cf27328a989352f888e752744bbc9
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-01-31 16:48:05 +0900

    김형민 : BOJ 1149 RGB거리
    - 144ms
    - DP

---
+++ week2/형민/BJ1149_RGB거리.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int arr[][] = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int dp[][] = new int[n][n];
+        dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+        }
+
+        int result = Math.min(dp[n - 1][0], dp[n - 1][1]);
+        result = Math.min(result, dp[n - 1][2]);
+        System.out.println(result);
+    }
+}
+
