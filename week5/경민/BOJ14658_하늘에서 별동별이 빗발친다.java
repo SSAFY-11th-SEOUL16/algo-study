a2d87309794942b84da0c548b83d273b91bb5890
gangintheremark <nkh9587@naver.com>
2024-02-21 22:19:27 +0900

    남경민 : BOJ 14658 하늘에서 별동별이 빗발친다
    - 144ms
    
    완전탐색으로 구현하면 시간초과가 난다. ( 1 ≤ N, M ≤ 500,000 ) 따라서 별의 좌표를 저장한 후 삼중 for문을 통해 해결할 수 있었다.

--- week5/경민/BOJ14658_하늘에서 별동별이 빗발친다.java
+++ week5/경민/BOJ14658_하늘에서 별동별이 빗발친다.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
