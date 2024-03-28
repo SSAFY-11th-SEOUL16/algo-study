0e558c7e6bae5a704a7f3240f96ab196543cd58a
정지환 <64758861+stophwan@users.noreply.github.com>
2024-03-28 09:21:28 +0900

    줄세우기 주석 추가

--- week10/지환/BJ2631_줄세우기.java
+++ week10/지환/BJ2631_줄세우기.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+//Java8(76ms)
+//결국 구해야하는 건 움직여야하는 '사람의 수'
+//최장 증가하는 수열을 구하고 전체 인원수에서 빼주면 움직여야하는 '사람의 수'가 구해진다.
 public class BJ2631_줄세우기 {
 
 	public static void main(String[] args) throws Exception {
