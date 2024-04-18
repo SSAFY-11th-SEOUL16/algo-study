ead9ba87610203fc3d056a04e5f48357f0be6f6a
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-04-18 12:05:49 +0900

    김형민 : BOJ 1644 소수의연속합

---
+++ week13/형민/BJ1644_소수의연속합.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        //소수 구하기
+        
+        
+        //경우의 수 찾기
+	}
+}
