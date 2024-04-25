451732a3c1190f0bb9cab578e12bee2ab616f530
남경민 <nkh9587@naver.com>
2024-04-25 11:46:06 +0900

    남경민 : BOJ 2342 Dance Dance Revolution
    - 496ms
    - DP (Top-down)
    
    ※ 블로그 참고하였습니다

---
+++ week14/경민/BOJ2342_Dance Dance Revolution.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int[] move;
+	static int[][][] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] line = br.readLine().split(" ");
+		move = new int[line.length - 1];
+		for (int i = 0; i < line.length - 1; i++) {
+			move[i] = Integer.parseInt(line[i]);
+		}
+
+		dp = new int[5][5][line.length];
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				Arrays.fill(dp[i][j], -1);
+			}
+		}
+		System.out.println(solve(0, 0, 0));
+	}
+
+	static int solve(int left, int right, int cnt) {
+
+		if (cnt == move.length)
+			return 0;
+
+		if (dp[left][right][cnt] != -1)
+			return dp[left][right][cnt];
+
+		// MAth.min(재귀경로1 + value , 재귀경로2 + value )
+		dp[left][right][cnt] = Math.min(solve(move[cnt], right, cnt + 1) + energy(left, move[cnt]),
+				solve(left, move[cnt], cnt + 1) + energy(right, move[cnt]));
+		return dp[left][right][cnt];
+	}
+
+	// 소모하는 힘
+	static int energy(int pos, int des) {
+		int num = Math.abs(pos - des);
+		if (pos == 0)
+			return 2;
+		else if (num == 0)
+			return 1;
+		else if (num == 1 || num == 3)
+			return 3;
+		else
+			return 4;
+	}
+}
